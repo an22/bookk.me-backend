ca1aea7ab12bd3d7f09e84aa557982964530d610
Michael Antiufieiev <antufeevmichael@gmail.com>
2025-04-21 01:56:26 +0300

    [ISSUE-37] add healthcheck

--- service/business/microservice/src/main/kotlin/com/book/business/microservice/route/BusinessRoute.kt
+++ service/business/microservice/src/main/kotlin/com/book/business/microservice/route/BusinessRoute.kt
@@ -1,9 +1,11 @@
 package com.book.business.microservice.route
 
 import com.book.business.microservice.route.api.createBusiness
+import com.book.business.microservice.route.api.healthCheck
 import io.ktor.server.routing.Routing
 
 
 fun Routing.businessRoute() {
+    healthCheck()
     createBusiness()
 }

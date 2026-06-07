2e3f5c2d480570e6faf7ad6cc556124caf4e44e0
Michael Antiufieiev <antufeevmichael@gmail.com>
2026-06-07 11:56:17 +0200

    [ISSUE-53] fix receiver registration

--- service/user/domain/impl/src/main/kotlin/com/bookk/user/domain/impl/event/UserEventHandler.kt
+++ service/user/domain/impl/src/main/kotlin/com/bookk/user/domain/impl/event/UserEventHandler.kt
@@ -2,7 +2,7 @@ package com.bookk.user.domain.impl.event
 
 import com.bookk.core.data.eventstreaming.EventHandler
 import com.bookk.core.data.eventstreaming.StandardEventConsumer
-import com.bookk.core.data.eventstreaming.registerReceiver
+import com.bookk.core.data.eventstreaming.registerResultReceiver
 import com.bookk.server.auth.client.AuthEvent
 import com.bookk.user.domain.api.operation.DeleteUser
 import kotlinx.coroutines.CoroutineScope
@@ -13,7 +13,7 @@ internal class UserEventHandler(
 ) : EventHandler {
     override fun start(scope: CoroutineScope) {
         consumer
-            .registerReceiver(AuthEvent.UserDeleted.TOPIC) { event: AuthEvent.UserDeleted ->
+            .registerResultReceiver(AuthEvent.UserDeleted.TOPIC) { event: AuthEvent.UserDeleted ->
                 deleteUser(event.userId)
             }
             .start(scope)
